4f14b754fb5e6b75e4cf26b559db249be63f60d7
Jeremy Okamoto <okamotojeremy@gmail.com>
2021-07-26 11:56:39 -0700

    a

---
+++ script.js
@@ -0,0 +1,19 @@
+//console.log("hi");
+var a = 9;
+var b = 12;
+var c = 9;
+function likeButton1() {
+    //console.log("hello");
+    a++;
+    document.querySelector("#likes1").innerHTML = a + " like(s)";
+}
+function likeButton2() {
+    //console.log("hello");
+    b++;
+    document.querySelector("#likes2").innerHTML = b + " like(s)";
+}
+function likeButton3() {
+    //console.log("hello");
+    c++;
+    document.querySelector("#likes3").innerHTML = c + " like(s)";
+}
